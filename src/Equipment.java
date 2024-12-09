5350e5d1cc0a039bc1c7762493b966e0c8134838
Nikhil Tole <ntole25@cgps.org>
2024-12-09 09:08:48 -0500

    Added classes Nikhil

---
+++ src/Equipment.java
@@ -0,0 +1,6 @@
+public class Equipment extends Item{
+
+
+
+
+}
---
+++ src/Stats.java
@@ -0,0 +1,10 @@
+public class Stats {
+    private int strength;
+    private int dexterity;
+    private int con;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
+
+
+}
