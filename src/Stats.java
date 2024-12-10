99a6e187f9fc2e853665e2f593d6f9c87458669b
Nikhil Tole <ntole25@cgps.org>
2024-12-10 09:59:30 -0500

    finishing lab

--- src/Stats.java
+++ src/Stats.java
@@ -7,5 +7,59 @@ public class Stats {
     private int hardiness;
     private int wisdom;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxHP() {
+        return maxHP;
+    }
+
+    public void setMaxHP(int maxHP) {
+        this.maxHP = maxHP;
+    }
+
+    public int getCurrentHP() {
+        return currentHP;
+    }
+
+    public void setCurrentHP(int currentHP) {
+        this.currentHP = currentHP;
+    }
+
+    public int getMaxMP() {
+        return maxMP;
+    }
+
+    public void setMaxMP(int maxMP) {
+        this.maxMP = maxMP;
+    }
+
+    public int getCurrentMP() {
+        return currentMP;
+    }
+
+    public void setCurrentMP(int currentMP) {
+        this.currentMP = currentMP;
+    }
+
+    public int getHardiness() {
+        return hardiness;
+    }
+
+    public void setHardiness(int hardiness) {
+        this.hardiness = hardiness;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
 }
